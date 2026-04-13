bf45837d608351ec95381c565acf10e666c62a66
DanielKriegerS <danielschifelbain@gmail.com>
2026-04-13 19:30:23 -0300

    V1_New: created generic title row

---
+++ app/src/main/java/com/danielks/shoppinglist/core/designsystem/component/TitleRow.kt
@@ -0,0 +1,25 @@
+package com.danielks.shoppinglist.core.designsystem.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TitleRow(title : String) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
