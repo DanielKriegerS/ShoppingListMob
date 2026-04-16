7b129c89b276db1f14b17dc9cc83746ebad128c7
DanielKriegerS <danielschifelbain@gmail.com>
2026-04-16 11:30:04 -0300

    V1_Refac: Adjusted header of create list

--- app/src/main/java/com/danielks/shoppinglist/feature/listdetail/component/ListHeader.kt
+++ app/src/main/java/com/danielks/shoppinglist/feature/listdetail/component/ListHeader.kt
@@ -16,6 +16,6 @@ fun ListHeader(
     Column(Modifier.fillMaxWidth().padding(16.dp)) {
         Text(name, style = MaterialTheme.typography.titleLarge)
         Spacer(Modifier.height(4.dp))
-        Text("Itens: $itemsCount • Marcados: $checkedCount", style = MaterialTheme.typography.bodyMedium)
+        Text("Total de itens: $itemsCount • No carrinho: $checkedCount", style = MaterialTheme.typography.bodyMedium)
     }
 }
